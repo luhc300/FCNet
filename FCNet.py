52131b9450c5a81386f3a708ffd6646df42579ae
BIBO <18516053895@163.com>
2017-09-01 21:54:30 +0800

    train_2_test

--- FCNet.py
+++ FCNet.py
@@ -3,8 +3,8 @@ Created by Haochuan Lu on 6/3/17.
 """
 import tensorflow as tf
 import numpy as np
-
-
+import pandas as pd
+from sklearn.metrics import roc_auc_score as ra_score
 def weight_variable(shape,name):
     initial = tf.truncated_normal(shape, stddev=1e-1)
     return tf.Variable(initial,name=name)
@@ -24,6 +24,7 @@ def L_variable(name,shape=None,init=None):
 
 
 def get_batch(X,y,batch_size):
+    '''
     N = X.shape[0]
     a = np.arange(N)
     np.random.shuffle(a)
@@ -32,7 +33,25 @@ def get_batch(X,y,batch_size):
     XX = X[mask]
     yy = y[mask]
     return XX, yy
+    '''
 
+    default_1 = np.where(y == 1)[0]
+    default_0 = np.where(y == 0)[0]
+    np.random.shuffle(default_0)
+    np.random.shuffle(default_1)
+    random_default_0 = default_0[:batch_size]
+    random_default_1 = default_1[:batch_size]
+    # print(random_default_0)
+    under_sample_default_0 = X[random_default_0]
+    under_sample_default_1 = X[random_default_1]
+    y_0 = y[random_default_0]
+    y_1 = y[random_default_1]
+    under_sample_X = np.concatenate([under_sample_default_0, under_sample_default_1])
+    under_sample_y = np.concatenate([y_0,y_1])
+    # print(len(under_sample_indices))
+    y_dummies = np.array(pd.get_dummies(under_sample_y))
+    #print(y_dummies)
+    return under_sample_X, y_dummies
 
 class FCNet:
     def __init__(self, input_dim, hidden_dims, output_dim):
@@ -59,24 +78,27 @@ class FCNet:
         #with tf.Session() as sess:
             #sess.run(tf.initialize_all_variables())
 
-    def train(self, X, y, reg=1e-5, learning_rate=1e-7, batch_size=100,trained_model_path = None):
+    def train(self, X, y, reg=1e-7, learning_rate=1e-7, batch_size=100,trained_model_path = None):
         layer_results = []
         X_input = tf.placeholder("float",shape = [None,self.input_dim])
         y_input = tf.placeholder("float",shape = [None,self.output_dim])
         lr = tf.placeholder("float")
         X0 = tf.convert_to_tensor(X_input)
         layer_results.append(X0)
-        reg_tern = tf.convert_to_tensor(0.0)
-        for i in range(self.num_layers):
+        reg_tern = tf.convert_to_tensor(reg)
+        for i in range(self.num_layers-1):
             X_output = tf.nn.relu(tf.matmul(layer_results[i], self.params_W[i]) + self.params_b[i])
             X_output_drop = tf.nn.dropout(X_output,1)
+
             layer_results.append(X_output_drop)
+
             reg_tern = reg_tern + tf.reduce_sum(self.params_W[i]*self.params_W[i])
-        y_result = tf.nn.softmax(layer_results[self.num_layers])
-        cross_entropy = -tf.reduce_sum(y_input * tf.log(y_result), name='cross_entropy')
-        loss = cross_entropy #+ reg_tern * reg * 0.5
+        y_result = tf.nn.softmax(tf.matmul(layer_results[self.num_layers-1],self.params_W[self.num_layers - 1]) + self.params_b[self.num_layers - 1])
+        cross_entropy = -tf.reduce_sum(y_input * tf.log(y_result + 1e-10), name='cross_entropy')
+        loss = cross_entropy + reg_tern * reg * 0.5
         train_step = tf.train.AdamOptimizer(lr).minimize(loss)
         prediction = tf.argmax(y_result, 1)
+        ori_prediction = tf.argmax(y_input, 1)
         correct_prediction = tf.equal(prediction, tf.argmax(y_input, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"), name='accuracy')
         cur_learning_rate = learning_rate
@@ -94,17 +116,23 @@ class FCNet:
 
 
             #print(sess.run(self.params_W[0]))
-            for j in range(1500):
+            for j in range(120000):
                 X_batch, y_batch = get_batch(X, y, batch_size)
                 if j % 100 == 0:
+                    print("iter %d" %j)
                     print(sess.run(loss,feed_dict={ X_input: X_batch, y_input:y_batch }))
                     print(sess.run(accuracy,feed_dict={ X_input: X_batch, y_input:y_batch }))
+                    results = sess.run(prediction, feed_dict={X_input: X_batch, y_input:y_batch})
+                    ori_result = sess.run(ori_prediction, feed_dict={X_input: X, y_input:y_batch})
+                    #print(results.shape)
+                    rascore = ra_score(ori_result, results)
+                    print("ROC_AUC %f" %rascore)
                     #print(sess.run(self.params_W[0]))
                     #print(sess.run(layer_results[-1], feed_dict={X_input: X_batch, y_input: y_batch}))
                     #print(sess.run(y_result,feed_dict={X_input:X_batch}))
-                    cur_learning_rate = cur_learning_rate * 0.95
-                sess.run(train_step,feed_dict={ X_input: X_batch, y_input:y_batch ,lr: cur_learning_rate*0.95})
-            saver_path = self.saver.save(sess, "model/model_3.ckpt")
+                    cur_learning_rate = cur_learning_rate * 1
+                sess.run(train_step,feed_dict={ X_input: X_batch, y_input:y_batch ,lr: cur_learning_rate})
+            saver_path = self.saver.save(sess, trained_model_path)
             print("Model saved in file: ", saver_path)
             #print(sess.run(self.params_W[0]))
         self.trained = True
@@ -116,20 +144,21 @@ class FCNet:
         y_input = tf.placeholder("float", shape=[None, self.output_dim])
         X0 = tf.convert_to_tensor(X_input)
         layer_results.append(X0)
-        for i in range(self.num_layers):
+        for i in range(self.num_layers - 1):
             X_output = tf.nn.relu(tf.matmul(layer_results[i], self.params_W[i]) + self.params_b[i])
             layer_results.append(X_output)
-        y_result = tf.nn.softmax(layer_results[self.num_layers])
-        prediction = tf.argmax(y_result, 1)
-        correct_prediction = tf.equal(prediction, tf.argmax(y_input, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"), name='accuracy')
+        y_result = tf.nn.softmax(tf.matmul(layer_results[self.num_layers-1],self.params_W[self.num_layers - 1]) + self.params_b[self.num_layers - 1])
+        prediction = y_result[:,1]
+        #correct_prediction = tf.equal(prediction, tf.argmax(y_input, 1))
+        #accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"), name='accuracy')
         with tf.Session() as sess:
             if model_path is not None:
                 self.saver.restore(sess,model_path)
             #print(sess.run(self.params_b[1]))
             results = sess.run(prediction,feed_dict={X_input:X})
             if y is not None:
-                print(sess.run(accuracy,feed_dict={X_input:X,y_input:y}))
+                pass
+                #print(sess.run(accuracy,feed_dict={X_input:X,y_input:y}))
         return results
 
 
--- InputData.py
+++ InputData.py
@@ -7,7 +7,9 @@ import numpy as np
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import GradientBoostingClassifier
 
-def down_sample(df):
+
+def down_sample(df,batch_size):
+    '''
     number_default_1 = len(df[df.TARGET == 1])
     default_1 = np.array(df[df.TARGET == 1].index)
     default_0 = np.array(df[df.TARGET == 0].index)
@@ -19,56 +21,121 @@ def down_sample(df):
     # print(len(under_sample_indices))
     df_undersample = df.loc[under_sample_indices]
     return df_undersample
+    '''
+    number_default_1 = len(df[df.TARGET == 1])
+    default_1 = np.array(df[df.TARGET == 1].index)
+    default_0 = np.array(df[df.TARGET == 0].index)
+    np.random.shuffle(default_0)
+    np.random.shuffle(default_1)
+    random_default_0 = default_0[:batch_size]
+    random_default_1 = default_1[:batch_size]
+    #print(random_default_0)
+    under_sample_default_0 = random_default_0
+    under_sample_default_1 = random_default_1
+    under_sample_indices = np.concatenate([under_sample_default_1, under_sample_default_0])
+    # print(len(under_sample_indices))
+    np.random.shuffle(under_sample_indices)
+    df_undersample = df.loc[under_sample_indices]
+    return df_undersample
 
-
-def normalize(data):
-    divide = np.max(data,axis=0) - np.min(data,axis=0)
-    divide[divide==0] = 1
-    minus = np.min(data,axis=0)
+def normalize(data, d=None, m=None):
+    if d is None:
+        divide = np.max(data,axis=0) - np.min(data,axis=0)
+        divide[divide==0] = 1
+    else:
+        divide = d
+    if m is None:
+        minus = np.min(data,axis=0)
+    else:
+        minus = m
     result = data - minus
     result /= divide
     #print(result)
-    return result
+    return result, divide, minus
+
 
 def feature_select(X,y):
-    mask = SelectFromModel(GradientBoostingClassifier()).fit(X,y).get_support()
+    mask = SelectFromModel(GradientBoostingClassifier(),'1.25*mean').fit(X,y).get_support()
     return mask
+
+def data_wash(data):
+    for index,col in data.iteritems():
+        mean = np.mean(col)
+        for i in range(0,len(col)):
+            if np.abs(col.iloc[[i][0]]) > 900000 :
+                col.iloc[[i][0]] = mean
+    return data
+
+def data_wash_2(data):
+    for i in range(data.shape[1]):
+        mean = np.mean(data[:,i])
+        data[np.abs(data[:,i])>666666,i] = mean
+    return data
+
+def data_generate(train,test):
+    index = train.shape[0]
+    all = np.concatenate([train,test])
+    all = data_wash_2(all)
+    all = normalize(all)[0]
+    train_gen = all[:index]
+    test_gen = all[index:]
+    return train_gen, test_gen
 
 
 data_train = pd.read_csv('data/train.csv',index_col='ID')
+'''
+data_train = data_wash(data_train)
 XX = data_train.drop(['TARGET'],axis=1)
 X_f = np.array(XX)
 y_f = np.array(data_train['TARGET'])
-data_feature = feature_select(X_f,y_f)
+#data_feature = feature_select(X_f,y_f)
 
-XX = XX.iloc[:,data_feature]
+#XX = XX.iloc[:,data_feature]
 XX['TARGET'] = data_train['TARGET']
 data_train = XX
 #print(data_train.columns)
 input_dim = data_train.shape[1] -1
 print(input_dim)
-fc_net = FCNet.FCNet(input_dim,[70,80,90,100,110,120,130,140,150,160,170,180,190,200,190,180,170,160,150,140,130,120,110,100,90,80,70,60,50,40,30,20],2)
+fc_net = FCNet.FCNet(input_dim,[500,400,300,200,100,50],2)
 lr = 3e-5
-for i in range(30):
-    data = down_sample(data_train)
-    X = data.drop(['TARGET'],axis=1)
-    yy = data['TARGET']
-    y = pd.get_dummies(yy)
-    X_batch = np.array(X,dtype='float')
-    y_batch = np.array(y,dtype='float')
-    X_batch = normalize(X_batch)
-    fc_net.train(X_batch, y_batch,learning_rate=lr,batch_size=800,trained_model_path='model/model_3.ckpt')
-    lr *=0.95
+
+data = data_train
+X = data.drop(['TARGET'],axis=1)
+yy = data['TARGET']
+y = pd.get_dummies(yy)
+X_batch = np.array(X,dtype='float')
+y_batch = np.array(yy,dtype='float')
+X_batch, divide, minus = normalize(X_batch)
+print(X_batch)
+#fc_net.train(X_batch, y_batch,learning_rate=lr,batch_size=800,trained_model_path='model/model_3.ckpt')
+lr *=0.85
 
 data_test = pd.read_csv('data/test.csv',index_col='ID')
-
-data_test = data_test.iloc[:,data_feature]
+data_test = data_wash(data_test)
+#data_test = data_test.iloc[:,data_feature]
 
 
 X_test = data_test
 X_array = np.array(X_test)
-X_array = normalize(X_array)
+X_array, divide2, minus2 = normalize(X_array, divide, minus)
 y_test = fc_net.predict(X_array,model_path='model/model_3.ckpt')
 X_test['TARGET'] = y_test
 result = X_test[['TARGET']]
+result.to_csv('data/predictions.csv')
+'''
+data_test = pd.read_csv('data/test.csv',index_col='ID')
+XX = data_train.drop(['TARGET'],axis=1)
+X_f = np.array(XX)
+y_f = np.array(data_train['TARGET'])
+X_t = np.array(data_test)
+X_fg, X_tg = data_generate(X_f, X_t)
+input_dim = X_fg.shape[1]
+print(input_dim)
+fc_net = FCNet.FCNet(input_dim,[500,50],2)
+#fc_net.train(X_fg, y_f,learning_rate=1e-4,batch_size=800,trained_model_path='model/model_4.ckpt')
+y_f_d = np.array(pd.get_dummies(y_f))
+y_test = fc_net.predict(X_tg,model_path='model/model_3.ckpt')
+print(y_test)
+data_test['TARGET'] = y_test
+result = data_test[['TARGET']]
 result.to_csv('data/predictions.csv')
